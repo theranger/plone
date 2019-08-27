71d5cfdac3c7be3bf0cd32893c6c243f69fc26f2
Ivari Horm <ivari@baltnet.ee>
2019-08-27 23:28:25 +0300

    Use HTTPS

--- footer.php
+++ footer.php
@@ -2,11 +2,16 @@
 	<div class="bottom">
 		<div class="footer">
 			<div style="float:left;">
-				<a rel="license" href="http://creativecommons.org/licenses/by-nd/3.0/">
-					<img alt="Creative Commonsi litsents" style="border-width:0" src="http://i.creativecommons.org/l/by-nd/3.0/80x15.png" />
+				<a rel="license" href="https://creativecommons.org/licenses/by-nd/3.0/">
+					<img alt="Creative Commonsi litsents" style="border-width:0"
+						 src="https://i.creativecommons.org/l/by-nd/3.0/80x15.png"/>
 				</a>
 				<br />
-				Siin avaldatud materjalid on litsentseeritud <a rel="license" href="http://creativecommons.org/licenses/by-nd/3.0/">Creative Commonsi Autorile viitamine + Tuletatud teoste keeld 3.0 Jurisdiktsiooniga sidumata litsentsiga</a>.
+				Siin avaldatud materjalid on litsentseeritud
+				<a rel="license" href="https://creativecommons.org/licenses/by-nd/3.0/">
+					Creative Commonsi Autorile viitamine + Tuletatud teoste keeld 3.0 Jurisdiktsiooniga sidumata
+					litsentsiga
+				</a>.
 			</div>
 			<div style="float:right; text-align: right;">
 				Plone-style design made by risk.ee<br />
@@ -14,8 +19,8 @@
 			</div>
 		</div>
 	</div>
-	
+
 	<?php wp_footer(); ?>
-	
-</body>
+
+	</body>
 </html>
