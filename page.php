664b6be21efffab6ad5fd7e88ffc54cd6773bb6d
Ivari Horm <ivari@baltnet.ee>
2019-08-27 23:26:37 +0300

    Upgrade to attachment v3 plugin

--- page.php
+++ page.php
@@ -11,53 +11,52 @@
 			$content = str_replace(']]>', ']]&gt;', $content);
 			if(empty($content)):
 		?>
-			
-			
-			<?php
+
+
+				<?php
 				$last_query = $wp_query;
 				$wp_query = new WP_Query();
 				$wp_query->query('posts_per_page=-1&orderby=menu_order&order=ASC&post_type=page&post_parent='.get_the_ID());
 			?>
-			
+
 			<?php if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
 			<div class="list">
 				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 				<?php the_excerpt(); ?>
 			</div>
 			<?php endwhile; $wp_query = $last_query; wp_reset_postdata(); else: ?>
-			<?php endif; ?>		
-		
+			<?php endif; ?>
+
 		<?php else: ?>
 			<?php echo $content; ?>
 		<?php endif; ?>
-		
+
 		<?php
-			if(function_exists("attachments_get_attachments")) {
-				$attachments = attachments_get_attachments();
-				$total_attachments = count($attachments);
+		if (class_exists("Attachments")) {
+			$attachments = new Attachments('attachments');
 			}
-			if($total_attachments > 0):
+		if ($attachments->exist()):
 		?>
 		<div class="attachments">
 			<div class="title">Seonduv sisu</div>
 			<ul>
-				<?php for($i=0; $i<$total_attachments; $i++): ?>
+				<?php while ($attachments->get()): ?>
 					<?php
-						$img = "icons/".basename($attachments[$i]['mime']).".png";
+					$img = "icons/" . basename($attachments->subtype()) . ".png";
 						if(!is_file(dirname(__FILE__)."/".$img)) $img = "icons/application.png";
 					?>
 					<li style="background-image: url(<?php echo  get_bloginfo('template_directory').'/'.$img; ?>);">
-						<a href="<?php echo $attachments[$i]['location'] ?>"><?php echo $attachments[$i]['title'] ?></a>
-						<p><?php echo $attachments[$i]['caption'] ?></p>
+						<a href="<?php echo $attachments->url() ?>"><?php echo $attachments->field('title') ?></a>
+						<p><?php echo $attachments->field('caption') ?></p>
 					</li>
-				<?php endfor; ?>
+				<?php endwhile; ?>
 			</ul>
 		</div>
-		
+
 		<?php endif; ?>
-			
-		<?php if(!empty($content)) comments_template(); ?> 
-		
+
+		<?php if (!empty($content)) comments_template(); ?>
+
 	</div>
 
 <?php get_footer(); ?>
