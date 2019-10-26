5b2784a46a8acf3f9e68ad607637f77d09558556
The Ranger <ranger@risk.ee>
2019-10-26 17:06:05 +0300

    Use two-column layout for index page as well.

--- templates/page-index.php
+++ templates/page-index.php
@@ -1,8 +1,4 @@
-<aside>
-	<?php if (is_active_sidebar("left")) dynamic_sidebar("left"); ?>
-</aside>
-
-<main>
+<main class="two-columns">
 	<?php if (have_posts()) while (have_posts()) : the_post(); ?>
 
 		<article>
